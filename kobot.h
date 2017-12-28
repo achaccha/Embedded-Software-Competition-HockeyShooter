bd2e1d3f4093341f825511cda2d4968a679f92c6
achaccha <minjun12378@gmail.com>
2017-12-28 13:07:10 +0900

    2017년 임베디드 소프트웨어 경진대회 하키
    Signed-off-by: achaccha <minjun12378@gmail.com>

---
+++ kobot.h
@@ -0,0 +1,11 @@
+#ifndef _KOBOT_HEADER_
+#define _KOBOT_HEADER_
+
+//#include "curling.h"
+//#include "hockey_passer.h"
+#include "hockey_shooter.h"
+#include "RobotProtocol.h"
+#include <pthread.h>
+
+#endif
+
